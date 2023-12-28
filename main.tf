43850b15fe31d57c43f12b58e16a9cd653ad6d2f
seshanb <125367985+seshanb@users.noreply.github.com>
2023-12-28 23:14:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "MyS3Bucket"
-    Environment = "Production"
+    Name        = var.tag_name
   }
 }
